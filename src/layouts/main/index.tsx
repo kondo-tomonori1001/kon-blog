54ee4b4f99fb7e64d0fb644e4a2973ae1a18c58b
kondo tomonori <tomonorik1001@gmail.com>
2021-03-11 22:31:28 +0900

    :tada:

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import { Header } from '../../components/Header/Header';
-import { getPostData } from 'src/lib/posts';
+import { existsGaId, GA_ID } from 'src/lib/gaTag'
+
 
 type Props = {
   children:React.ReactNode,
@@ -16,6 +17,23 @@ export const MainLayout:React.FC<Props> = ({children,home,page}):JSX.Element => 
         <title>{!home ? (`${page} | `):""}MyBlog</title>
         <meta charSet="utf-8"></meta>
         <meta property="og:type" content="website" />
+        {/* Google Analytics */}
+        {existsGaId && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+                }}
+              />
+            </>
+          )}
       </Head>
       <Header />
       <main className="bg-blue-100 text-sm md:text-base dark:bg-gray-800 dark:text-white min-h-screen">
