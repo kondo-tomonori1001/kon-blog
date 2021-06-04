afad63e300efc8c1cde1db05d5842719b04509ed
kondo tomonori <tomonorik1001@gmail.com>
2021-06-05 01:27:26 +0900

    🎉

--- src/components/ProfCard/index.tsx
+++ src/components/ProfCard/index.tsx
@@ -1,13 +1,18 @@
 import styles from "src/components/ProfCard/index.module.css";
+import clsx from "clsx";
+
 
 export const ProfCard = () => {
   return(
-    <div className=" bg-white dark:bg-gray-700">
-      <img className={styles.profBg} src="/images/prof_bg.png" alt="" />
-      <img className={styles.logo} src="/images/icon.webp" alt="" />
+    <div className="bg-white dark:bg-gray-700 mx-8 xl:mx-0 flex xl:block">
+      <div className="relative">
+        <img className={styles.profBg} src="/images/prof_bg.png" alt="" />
+        <img className={clsx(styles.logo, "hidden xl:block")} src="/images/icon.webp" alt="" />
+      </div>
       <div className="p-4">
         <p className="mb-4 text-center">KON BLOG</p>
-        <p>テキストテキストテキストテキストテキスト</p>
+        <p>地方WEB制作会社のフロントエンドエンジニアです。</p>
+        <p>主にWEB制作やVue.js、React、Wordpress（ブロックエディタ）について学んだことなどを発信します。</p>
       </div>
     </div>
   )
