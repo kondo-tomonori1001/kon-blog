b201d7e68cd3c07e3b8901987cceec8af5c157db
kondo tomonori <tomonorik1001@gmail.com>
2021-07-28 05:01:59 +0900

    🔧

--- src/components/ProfCard/index.tsx
+++ src/components/ProfCard/index.tsx
@@ -6,8 +6,8 @@ export const ProfCard = () => {
   return(
     <div className="bg-white dark:bg-gray-700 mx-8 xl:mx-0">
       <div className="relative">
-        <img className={styles.profBg} src="/images/prof_bg.jpeg" alt="" />
-        <img className={clsx(styles.logo)} src="/images/icon.webp" alt="" />
+        <img className={styles.profBg} src="/images/prof_bg.png" alt="" />
+        <img className={clsx(styles.logo)} src="/images/icon.jpeg" alt="" />
       </div>
       <div className="p-4">
         <p className="mb-4 text-center font-bold">KON BLOG</p>
